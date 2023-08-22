b030124055beab9f82888a492939225b1586ef67
Salam4nder <kamran.personal@icloud.com>
2023-08-22 21:10:48 +0200

    Add viper

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,17 @@
 package config
 
 import (
-	"github.com/plaid/go-envvar/envvar"
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
 )
 
 // App holds the application-wide configuration.
 type App struct {
+	Name       string
 	HTTPServer *HTTPServer
-	GRPCServer *GRPCServer
 }
 
 // HTTPServer holds the configuration for the HTTP server.
@@ -16,19 +20,40 @@ type HTTPServer struct {
 	Port string
 }
 
-// GRPCServer holds the configuration for the gRPC server.
-type GRPCServer struct {
-	Host string
-	Port string
-}
-
 // New returns the application-wide configuration.
 func New() (*App, error) {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".../config")
+	viper.AutomaticEnv()
+	viper.SetConfigType("yaml")
+
 	var cfg App
 
-	if err := envvar.Parse(&cfg); err != nil {
+	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
 	}
 
 	return &cfg, nil
+}
+
+// Watch watches for changes in the configuration file and updates the configuration accordingly.
+// Stops watching if an error occurs while unmarshalling to avoid weird behavior.
+func (x App) Watch(logger zerolog.Logger) {
+	for {
+		time.Sleep(10 * time.Second)
+		viper.WatchConfig()
+
+		if err := viper.Unmarshal(&x); err != nil {
+			logger.Error().Msgf("config: Error parsing config file, aborting... %s", err)
+
+			return
+		}
+
+		logger.Info().Msg("Config reloaded")
+	}
+}
+
+// Addr returns the address of the configured HTTP server.
+func (x *HTTPServer) Addr() string {
+	return fmt.Sprintf("%s:%s", x.Host, x.Port)
 }
