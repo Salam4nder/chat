2112ba63f5545004ad7a7004caf06d114ed13633
Salam4nder <kamran.personal@icloud.com>
2023-08-22 21:10:17 +0200

    Update message.go

--- internal/chat/message.go
+++ internal/chat/message.go
@@ -1,13 +1,17 @@
 package chat
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 // Message defines the message structure.
 type Message struct {
 	ID        uuid.UUID
+	Type      int
 	ChannelID uuid.UUID
-	Body      interface{}
+	Body      []byte
 	Author    string
+	Timestamp time.Time
 }
