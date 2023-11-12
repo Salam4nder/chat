a46371d6c15a52060b1c3d7c747c8a8907b856e8
Salam4nder <kamran.personal@icloud.com>
2023-11-13 00:21:28 +0100

    Remove custom message types for now

--- internal/chat/message.go
+++ internal/chat/message.go
@@ -1,32 +1,15 @@
 package chat
 
 import (
-	"time"
-
 	"github.com/google/uuid"
-)
-
-type MessageType int
-
-const (
-	// MessageTypeText is a text message.
-	MessageTypeText MessageType = 1
-	// MessageTypeImage is an image message.
-	MessageTypeImage MessageType = 2
-	// MessageTypeVideo is a video message.
-	MessageTypeVideo MessageType = 3
-	// MessageTypeAudio is an audio message.
-	MessageTypeAudio MessageType = 4
-	// MessageTypeFile is an arbitrary file message.
-	MessageTypeFile MessageType = 5
 )
 
 // Message defines the message structure.
 type Message struct {
 	ID        uuid.UUID
-	Type      MessageType
+	Type      int
 	SessionID uuid.UUID
 	Body      []byte
 	Author    string
-	Timestamp time.Time
+	Timestamp string
 }
