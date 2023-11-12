3b94fcf31895e6ed05c6251babc1b3e6c26c638e
Salam4nder <kamran.personal@icloud.com>
2023-11-12 03:11:55 +0100

    Add zerolog logs

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -1,13 +1,13 @@
 package http
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/Salam4nder/chat/internal/chat"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+	"github.com/rs/zerolog/log"
 )
 
 func handleWS(w http.ResponseWriter, r *http.Request) {
@@ -20,9 +20,7 @@ func handleWS(w http.ResponseWriter, r *http.Request) {
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		// handle this better.
-		log.Println(err)
-
+		log.Error().Err(err).Msg("http: failed to upgrade connection")
 		return
 	}
 
@@ -30,8 +28,7 @@ func handleWS(w http.ResponseWriter, r *http.Request) {
 
 	roomID, err := uuid.Parse(queryValues.Get("roomID"))
 	if err != nil {
-		log.Println(err)
-
+		log.Error().Err(err).Msg("http: failed to parse connection")
 		return
 	}
 
@@ -53,4 +50,6 @@ func handleWS(w http.ResponseWriter, r *http.Request) {
 	go sess.Read()
 	go sess.Write()
 	room.Join <- sess
+
+	return
 }
