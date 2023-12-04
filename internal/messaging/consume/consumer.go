7d032fca59fb6e2c34894c301e015a092e43d8c3
Salam4nder <kamran.personal@icloud.com>
2023-12-04 02:27:32 +0100

    Add consumer

---
+++ internal/messaging/consume/consumer.go
@@ -0,0 +1,58 @@
+package consume
+
+import (
+	"context"
+
+	"github.com/rs/zerolog/log"
+	"github.com/segmentio/kafka-go"
+)
+
+var _ Consumer = (*KafkaConsumer)(nil)
+
+// Consumer is an abstraction for consuming messages from a topic.
+type Consumer interface {
+	Consume(ctx context.Context, topic string, partition int, offset int64) (chan *kafka.Message, error)
+}
+
+// KafkaConsumer implements the Consumer interface.
+type KafkaConsumer struct {
+	reader *kafka.Reader
+}
+
+// NewKafkaConsumer creates a new KafkaConsumer.
+func NewKafkaConsumer(brokers []string, groupID string) *KafkaConsumer {
+	return &KafkaConsumer{
+		reader: kafka.NewReader(kafka.ReaderConfig{
+			Brokers: brokers,
+			GroupID: groupID,
+		}),
+	}
+}
+
+func (x *KafkaConsumer) Consume(ctx context.Context, topic string, partition int, offset int64) (chan *kafka.Message, error) {
+	if err := x.reader.SetOffset(offset); err != nil {
+		return nil, err
+	}
+	mChan := make(chan *kafka.Message, 1)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				log.Info().Msg("consume: context cancelled")
+				close(mChan)
+				x.reader.Close()
+			default:
+			}
+
+			message, err := x.reader.ReadMessage(ctx)
+			if err != nil {
+				log.Error().Err(err).Msg("consume: failed to read message")
+				continue
+			}
+			mChan <- &message
+		}
+	}()
+
+	return mChan, nil
+}
