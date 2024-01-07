d284d0718a83efe6eef6c02b4c03258a06bfccfd
Salam4nder <kamran.personal@icloud.com>
2024-01-07 01:54:08 +0100

    Add MessageType -> string conversion

--- internal/chat/message.go
+++ internal/chat/message.go
@@ -14,3 +14,21 @@ type Message struct {
 	Author    string
 	Timestamp string
 }
+
+// TypeString returns a friendly string representation of the message type.
+func (x *Message) TypeString() string {
+	switch x.Type {
+	case 1:
+		return "TextMessage"
+	case 2:
+		return "BinaryMessage"
+	case 8:
+		return "CloseMessage"
+	case 9:
+		return "PingMessage"
+	case 10:
+		return "PongMessage"
+	default:
+		return "Unknown"
+	}
+}
