1248e4f88666294fe59a06ed0c6c087d4dd57f48
Salam4nder <kamran.personal@icloud.com>
2024-01-13 23:12:40 +0100

    Add CreateUserInRoom test

--- internal/db/keyspace/chat/user_test.go
+++ internal/db/keyspace/chat/user_test.go
@@ -1,3 +1,24 @@
 //go:build testdb
 
 package chat
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/gocql/gocql"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_CreateUserInRoom(t *testing.T) {
+	timeNow := time.Now()
+
+	params := UserInRoom{
+		UserID: gocql.UUIDFromTime(timeNow),
+		RoomID: gocql.UUIDFromTime(timeNow),
+	}
+
+	err := testUserRepo.CreateUserInRoom(context.Background(), params)
+	require.NoError(t, err)
+}
