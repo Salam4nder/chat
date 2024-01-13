5474eb6a8243d987ff47cacab2e29a9e309622dd
Salam4nder <kamran.personal@icloud.com>
2024-01-13 22:35:00 +0100

    Update test to not depend on ReadMessagesByRoomID

--- internal/db/keyspace/chat/message_test.go
+++ internal/db/keyspace/chat/message_test.go
@@ -12,31 +12,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// func TestScyllaRepository_Create(t *testing.T) {
-// 	tests := []struct {
-// 		name        string
-// 		params      *Message
-// 		want        *Message
-// 		wantErr     bool
-// 		requiredErr error
-// 	}{
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got, err := TestScyllaConn.Create()
-
-// 		})
-// 	}
-// }
-var ctx context.Context = context.Background()
-
 func TestScyllaKeyspace_CreateMessageByRoom(t *testing.T) {
+	var ctx context.Context = context.Background()
+
 	t.Run("Success", func(t *testing.T) {
 		timeNow := time.Now().UTC()
 
 		t.Cleanup(func() {
-			// err := TestScyllaConn.Session().ExecStmt("DELETE from message.message_by_room")
-			// assert.NoError(t, err)
+			err := TestScyllaConn.Session().Query("TRUNCATE chat.message_by_room").Exec()
+			assert.NoError(t, err)
 		})
 
 		params := CreateMessageByRoomParam{
@@ -50,14 +34,34 @@ func TestScyllaKeyspace_CreateMessageByRoom(t *testing.T) {
 		err := TestScyllaConn.CreateMessageByRoom(ctx, params)
 		require.NoError(t, err)
 
-		messages, err := TestScyllaConn.ReadMessagesByRoom(ctx, params.RoomID)
-		require.NoError(t, err)
-		require.Equal(t, 1, len(messages))
+		query := `SELECT id, data, type, sender, room_id, time 
+              FROM chat.message_by_room 
+              WHERE room_id = ?`
 
-		assert.Equal(t, params.Data, messages[0].Data)
-		assert.Equal(t, params.Type, messages[0].Type)
-		assert.Equal(t, params.Sender, messages[0].Sender)
-		assert.Equal(t, params.RoomID, messages[0].RoomID)
-		assert.Equal(t, params.Timestamp.Format(time.DateTime), messages[0].Time.Format(time.DateTime))
+		messages := make([]Message, 0)
+		scanner := TestScyllaConn.Session().Query(query, params.RoomID).Iter().Scanner()
+		for scanner.Next() {
+			var message Message
+
+			err := scanner.Scan(
+				&message.ID,
+				&message.Data,
+				&message.Type,
+				&message.Sender,
+				&message.RoomID,
+				&message.Time,
+			)
+			assert.NoError(t, err)
+
+			messages = append(messages, message)
+		}
+
+		require.Equal(t, 1, len(messages))
+		m := messages[0]
+		assert.Equal(t, params.Data, m.Data)
+		assert.Equal(t, params.Type, m.Type)
+		assert.Equal(t, params.Sender, m.Sender)
+		assert.Equal(t, params.RoomID, m.RoomID)
+		assert.Equal(t, params.Timestamp.Format(time.DateTime), m.Time.Format(time.DateTime))
 	})
 }
